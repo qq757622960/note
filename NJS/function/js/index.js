7a21df0f2debf6bbeef97ed6162d025e1bb82106
qq757622960 <qq757622960>
2019-09-11 12:15:22 +0800

    扩充类型的功能
    扩充类型的功能

--- NJS/function/js/index.js
+++ NJS/function/js/index.js
@@ -1,12 +1,29 @@
-$('body').on('click', '*[ys-event]', function (e) {
-    var othis = $(this), methid = othis.attr('ys-event');
-    events[methid] ? events[methid].apply(this, [othis, e]) : '';
-});
 
-// 事件表里面存放动作
 
-var events = {
-    detail: function (othis, e) {
-        console.log('详情');
-    }
-};
+(function () {
+
+    // 给字符串、数组、布尔值、数字、正则表达式 添加方法
+
+
+    // 根据数字的正负值来判断是使用 Math.ceiling 还是 Math.floor
+
+    // Number.prototype.integer = function () {  
+    //     return Math[this < 0 ? 'ceil' : 'floor'](this);
+    // };
+
+
+    // 给字符串添加一个去除收尾空格的方法
+
+    // String.prototype.trim = function () {  
+    //     return this.replace(/^\s+|\s+$/g, '');
+    // };
+
+    // Function.prototype.method = function (name, func) {  
+    //     if (!this.prototype[name]) {
+    //         this.prototype[name] = func;
+    //     }
+    //     return this;
+    // };
+
+
+})();
